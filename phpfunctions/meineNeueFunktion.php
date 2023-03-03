99684606ad4fbf42055e5ac11b61d302f7a6989c
sweimann <stefan.weimannjobrouter.com>
2023-03-03 14:31:52 +0100

    Neue Datei

---
+++ phpfunctions/meineNeueFunktion.php
@@ -0,0 +1,11 @@
+<?php
+
+class className extends JobRouter\Engine\Runtime\PhpFunction\RuleExecutionFunction
+{
+	public function execute($rowId = null)
+	{
+        $this->save();
+        //Kommentare
+	}
+}
+?>
